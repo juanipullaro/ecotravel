2af6277797345b5061a8fc5b6bad3b782df09fe9
juanipullaro <juanipullaro_97@hotmail.com>
2020-09-03 17:46:20 -0300

    changes in travels search

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/ecotravel'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hvgpayytgzdriz:8866ed90209b425b1564220054fcb02a4294dd90e9021bf4fd74f11d496a6783@ec2-54-235-192-146.compute-1.amazonaws.com:5432/d18fcqbriqdia7'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.debug=True
 db = SQLAlchemy(app)
